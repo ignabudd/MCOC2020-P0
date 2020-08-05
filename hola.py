50ff902fb989d49fec8d38ed5f03bc8e6e77c848
ignabudd <69213519+ignabudd@users.noreply.github.com>
2020-08-05 15:32:09 -0400

    archivo hola.py

---
+++ hola.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy 
+
+print ("Hola MCOC")
+
+nombre = "MCOC"
+dia = 3
+
+print ("Forma 1")
+print ("Hola Ignacia es el 3 de Agosto")
+
+print ("Forma 2")
+print ("Hola " + nombre + " es el " + str(dia) + " Agosto")
+ 
+print ("Forma 3")
+print ("Hola {nombre} es el {dia} de Agosto".format(dia= dia,nombre= nombre))
+
+print ("Forma 4")
+print (f"Hola {nombre} es el {dia} de Agosto") 
+
+print ("Ejemplo 2 - Forma 1")
+print ("Hola Hola Hola Hola Hola")
+
+print ("Ejemplo 2 - Forma 2")
+print ("Hola " * 50)
+
+git comit
