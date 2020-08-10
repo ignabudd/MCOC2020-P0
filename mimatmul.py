25c55f3b0874f6ec08bbfa95e202bc484d210eb2
Ignacia Budd <69213519+ignabudd@users.noreply.github.com>
2020-08-10 11:31:09 -0400

    Add files via upload

---
+++ mimatmul.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 10 09:30:35 2020
+
+@author: ignab
+"""
+
+import numpy as np
+
+def mimatmul(A,B):
+    print ("Implementar matmul a mano")
+    C = np.dot(A,B)
+    return C
+
+
+def mimatmul1(A,B):
+    print ("Implementar matmul a mano")
+    res =[]
+    for a in range(len(A)):
+        row = []
+        for b in range(len(A)):
+            row.append(0)
+        res.append(row)
+    
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+            for k in range(len(B)):
+                res[i][j] += A[i][k]*B[k][j]
+                
+    
+    return res
